311b1e15aaa76779a0b035ac52e486046cbbfb72
AlissonGusvos <gustavoalisson322@gmail.com>
2025-06-25 10:31:26 -0300

    Projeto iniciado. Front conectado

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+const PORT = 3000;
+
+/* CONFIG EJS */
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+const indexRoute = require("./routes/index");
+
+app.use('/',indexRoute);
+
+app.listen(PORT,()=>{
+    console.log(`Application running | ${PORT}`);
+})
